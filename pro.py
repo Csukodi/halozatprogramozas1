bec0dcff5eb9d2d25f1a0fea28775f03e62e8a8d
Csukodi <csukodicsaba12316@gmail.com>
2025-09-05 12:15:55 +0200

    done

--- pro.py
+++ pro.py
@@ -1,1 +1,16 @@
-print("Szia")
+print("Szia", end=" ")
+print("Tibi")
+print("Szia", "tóth", "leves", "!", sep="")
+print("szia toth karcsi")
+nev = "toth karcsi"
+print(f"Szia {nev}")
+print("Szia", "peti", sep="\n", end="!") #\t
+#adattipusok
+# int()
+# float()
+# str()
+# bool()
+print()
+print(int("5"))
+print(float("5"))
+print(str("5.0"))
