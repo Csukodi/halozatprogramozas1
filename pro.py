0c191253b695901fde771899674441e973167e5f
Csukodi <csukodicsaba12316@gmail.com>
2025-09-05 12:17:30 +0200

    done

--- pro.py
+++ pro.py
@@ -1,16 +1,26 @@
 print("Szia", end=" ")
 print("Tibi")
-print("Szia", "tóth", "leves", "!", sep="")
-print("szia toth karcsi")
-nev = "toth karcsi"
-print(f"Szia {nev}")
-print("Szia", "peti", sep="\n", end="!") #\t
-#adattipusok
+print("Szia", "Tóth", "Karcsi", "!", sep="")
+print("Szia Tóth Karcsi!")
+nev = "Tóth Karcsi"
+print(f"Szia {nev}!")
+print("Szia", "Peti", sep="\n", end="!") #\t
+#adattípusok
 # int()
 # float()
 # str()
 # bool()
+# list()
+# set()
+# tuple()
 print()
 print(int("5"))
-print(float("5"))
-print(str("5.0"))
+print(float(5))
+print(type(str(5.0)))
+print(bool(-1))
+print(["hétfő", "kedd", "szerda"])
+napok = ["hétfő", "kedd", "szerda"]
+print(f"Napok: {napok}")
+nevek = ["Tibi", "Sanyi", "Tibi"]
+print(f"Nevek: {set(nevek)}")
+print(tuple([1,2,3]))
